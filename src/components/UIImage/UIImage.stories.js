fe3982ca20a9213eea32ca72fb841fbd01664f24
artempaskevichyan <qeeweewee@gmail.com>
2023-10-22 03:54:13 +0300

    UIImage 1.0.0, added UIImage with lazy loading but no size auto requests. Minor package changes

---
+++ src/components/UIImage/UIImage.stories.js
@@ -0,0 +1,14 @@
+import UIImage from "./UIImage.vue";
+
+export default {
+  component: UIImage,
+  title: "UIImage",
+  tags: ["autodocs"],
+  render: () => ({
+    components: { UIImage },
+    template:
+      "<UIImage src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png' data-src='https://segwitz.com/wp-content/uploads/2021/06/vuejs-development-malaysia.jpeg'/>",
+  }),
+};
+
+export const Default = {};
---
+++ src/components/UIImage/UIImage.vue
@@ -0,0 +1,53 @@
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  props: {
+    src: {
+      required: true,
+      type: String,
+    },
+    dataSrc: {
+      required: true,
+      type: String,
+    },
+    alt: {
+      type: String,
+    },
+    lazy: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup(props) {
+    const innerSrc = ref(props.src);
+    const img = ref(null);
+    if (props.lazy) {
+      const dataImage = new Image();
+      const observerCallback = () => {
+        dataImage.src = props.dataSrc;
+        dataImage.onload = () => {
+          innerSrc.value = dataImage.src;
+        };
+      };
+      const observerOptions = {
+        rootMargin: "100px",
+        threshold: 0.0,
+      };
+      const observer = new IntersectionObserver(
+        observerCallback,
+        observerOptions
+      );
+      onMounted(() => {
+        observer.observe(img.value);
+      });
+    }
+
+    return { innerSrc, img };
+  },
+};
+</script>
+
+<template>
+  <img :src="innerSrc" ref="img" />
+</template>
